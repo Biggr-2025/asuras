9e6fc2a75167e26050f91e2265e7bd0c7ec60eaf
Vakadu Vinod <vakadu10@gmail.com>
2025-02-18 19:12:36 +0530

    feat(ui): update status

---
+++ apps/ravana/app/(dashboard)/product-utils/_api/update-utils.ts
@@ -0,0 +1,33 @@
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
+
+import { HttpService } from '../../../../core/services';
+
+const updateProductUtils = async (name: string, payload: any) => {
+	const { apiKey, ...rest } = payload;
+	try {
+		const { data } = await HttpService.patch<ICommonTypes.IApiResponse<any>>(
+			`${process.env.NEXT_PUBLIC_BASE_PATH}/productUtil/${apiKey}/${name}`,
+			rest
+		);
+		return data;
+	} catch (err) {
+		throw new Error('Network Error. Please try again.');
+	}
+};
+
+export function useUpdateProductUtils(name: string) {
+	return useMutation({
+		mutationFn: (payload: any) => updateProductUtils(name, payload),
+		onSuccess: (data) => {
+			if (data.status === 'SUCCESS') {
+				toast.success('updated successfully.');
+			} else {
+				toast.error('Something went wrong. Please try again');
+			}
+		},
+		onError: (err) => {
+			toast.error(err.message);
+		},
+	});
+}
